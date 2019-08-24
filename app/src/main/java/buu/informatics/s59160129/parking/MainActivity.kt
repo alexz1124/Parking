a4fe83fe7cf718921e07d57c031ece725578534c
alexz1124 <alexz.waranyu@gmail.com>
2019-08-24 14:24:24 +0700

    ui

--- app/src/main/java/buu/informatics/s59160129/parking/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160129/parking/MainActivity.kt
@@ -1,12 +1,23 @@
 package buu.informatics.s59160129.parking
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import buu.informatics.s59160129.parking.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.statusOne.setOnClickListener {
+            test_Color(it)
+        }
+    }
+    private fun test_Color(box: View){
+        box.setBackgroundColor(Color.BLUE)
     }
 }
---
+++ app/src/main/java/buu/informatics/s59160129/parking/MyCar.kt
@@ -0,0 +1,3 @@
+package buu.informatics.s59160129.parking
+
+data class MyCar(var plate: String = "", var brand: String = "", var name : String = "")
